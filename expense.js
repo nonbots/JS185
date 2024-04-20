64a87d1e01f6657b355197a505e40f49f771071e
nonbots <nhantdbui@gmail.com>
2024-04-19 20:34:44 -0400

    Implement solution to disconnect from database issue
    Issue was due to readline connection being created globally.
    Moved the connect within the local scope of clear function.

--- expense.js
+++ expense.js
@@ -1,6 +1,5 @@
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
-const rl = readline.createInterface({ input, output });
 
 export function displayHelp(client) {
   console.log(`Commands:
@@ -119,6 +118,7 @@ export async function searchByMemo(memo,client){
 }
 
 export async function clear(client) {
+  const rl = readline.createInterface({ input, output });
   const response = await client.connect();
   console.log("Connection created");
   let  answer = await rl.question('Are you sure you want delete all records? (y/n)\n');
